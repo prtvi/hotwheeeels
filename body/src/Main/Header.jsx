5148813850fc0b9148774348e5f02d970d06a981
prtvi <prithvippv25@gmail.com>
2024-05-09 18:23:47 +0530

    add hyper link to social on page header

--- body/src/Main/Header.jsx
+++ body/src/Main/Header.jsx
@@ -3,12 +3,21 @@ import './Main.css';
 export default function Header(props) {
 	return (
 		<div className="header">
-			<h1 className="pf-500">Hot Wheeeels 🚗</h1>
+			<h1 className="pf-500">
+				<a
+					href="https://www.instagram.com/prtviv"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Prithvi's
+				</a>{' '}
+				Hot Wheeeels collection 🏎️
+			</h1>
 			<p
 				className="subtitle pif-200"
 				title="showcase of my hot wheels collection, includes other diecast brands too"
 			>
-				Showcase of my hot wheeeels collection
+				..includes other diecast brands too
 			</p>
 		</div>
 	);
